5a9fa6a570ccf533cbbde8d9418a290ddc191a0f
Chanisa01 <94674787+Chanisa01@users.noreply.github.com>
2022-10-11 15:06:58 +0700

    Add files via upload

---
+++ main.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <?php
+        if($_COOKIE["lang"]=="th"){
+            echo "ยินดีต้อนรับ";
+        }
+        elseif($_COOKIE["lang"]=="en"){
+            echo "welcome";
+        }
+        ?>
+    </body>
+</html>
---
+++ select.php
@@ -0,0 +1,8 @@
+<?php 
+    if(empty($_COOKIE["lang"])){
+        setcookie("lang",$_GET["language"],time()+3600*24);
+    }
+    if(isset($_COOKIE["lang"])){
+        setcookie("lang",$_GET["language"],time()+3600*24);
+    }
+?>
